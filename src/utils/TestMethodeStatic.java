8bdb06fc0a59c4b41f92104353f192f0c0f014b1
sopurdey <a_purdey@hotmail.com>
2021-11-08 10:35:47 +0100

    fin TP4

---
+++ src/utils/TestMethodeStatic.java
@@ -0,0 +1,17 @@
+package utils;
+
+public class TestMethodeStatic {
+
+	public static void main(String[] args) {
+
+		String chaine = "12";
+		
+		int a = Integer.parseInt(chaine);
+		int b = 25;
+		
+		int max = Integer.max(a, b);
+
+		System.out.println(max);
+	}
+
+}
