57943ab979d988d788dddd00e93fb80090fdff83
sopurdey <a_purdey@hotmail.com>
2021-11-09 14:26:15 +0100

    TP autonome partie 3

---
+++ src/fr/diginamic/essais/TestTheatre.java
@@ -0,0 +1,20 @@
+package fr.diginamic.essais;
+
+import fr.dignamic.entites.Theatre;
+
+public class TestTheatre {
+
+	public static void main(String[] args) {
+		
+		Theatre th1 = new Theatre("London Coliseum", 2300, 2100, 100_000);
+		System.out.println(th1);
+		
+		th1.inscrire(50, 50.0);
+		th1.inscrire(100, 50.0);
+		th1.inscrire(50, 50.0);
+		th1.inscrire(1, 50.0);
+
+		System.out.println(th1);
+	}
+
+}
---
+++ src/fr/dignamic/entites/Theatre.java
@@ -0,0 +1,61 @@
+package fr.dignamic.entites;
+
+public class Theatre {
+	private String nom;
+	private int maxCapacite;
+	private int totalInscrits;
+	private double recetteTotale;
+	
+	public void inscrire(int nombreClients, double prixPlace) {
+		if ((totalInscrits + nombreClients) <= maxCapacite) {
+			totalInscrits += nombreClients;
+		} else 
+			System.out.println("Erreur : la capactié max est atteinte");
+	}
+
+	public Theatre(String nom, int maxCapacite, int totalInscrits, double recetteTotale) {
+		super();
+		this.nom = nom;
+		this.maxCapacite = maxCapacite;
+		this.totalInscrits = totalInscrits;
+		this.recetteTotale = recetteTotale;
+	}
+
+	@Override
+	public String toString() {
+		return "Theatre " + nom + " : Capacité max : " + maxCapacite + " - Recette totale : " + recetteTotale;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public int getMaxCapacite() {
+		return maxCapacite;
+	}
+
+	public void setMaxCapacite(int maxCapacite) {
+		this.maxCapacite = maxCapacite;
+	}
+
+	public int getTotalInscrits() {
+		return totalInscrits;
+	}
+
+	public void setTotalInscrits(int totalInscrits) {
+		this.totalInscrits = totalInscrits;
+	}
+
+	public double getRecetteTotale() {
+		return recetteTotale;
+	}
+
+	public void setRecetteTotale(double recetteTotale) {
+		this.recetteTotale = recetteTotale;
+	}
+
+}
